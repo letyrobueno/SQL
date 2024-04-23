7e304867fdb0b6364b9b02c4e95daf3c7334f414
Letícia Rodrigues Bueno <letyrobueno@gmail.com>
2024-04-22 22:53:36 -0400

    Update interesting-queries.sql

--- interesting-queries.sql
+++ interesting-queries.sql
@@ -1,5 +1,6 @@
 -- Interesting queries
 
+-- Aggregations
 SELECT yr,
     mth,
     COUNT(*)
@@ -20,3 +21,23 @@ SELECT yr,
 FROM table1
 GROUP BY ROLLUP(yr, mth) -- years + months, years only, total (hierarchy intact)
 ORDER BY 1 DESC, 2 DESC;
+
+-- Recursive cte
+-- From: https://www.linkedin.com/posts/davidkfreitag_sql-dataengineering-activity-7187786005481967616-_Py8?utm_source=share&utm_medium=member_desktop
+-- Prints 1,2,3
+WITH RECURSIVE cte_1 AS (
+	-- base case
+	(SELECT 1 AS iteration)
+
+	UNION ALL
+
+	--each recursive case
+	(SELECT iteration + 1 AS iteration
+		FROM cte_1
+		WHERE iteration < 3 -- the stop sign
+	)
+)
+
+SELECT iteration
+FROM cte_1
+ORDER BY 1;
