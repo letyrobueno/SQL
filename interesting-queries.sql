efe29d956797c01d1c64dfca1efdc9ac815778c2
Letícia Rodrigues Bueno <letyrobueno@gmail.com>
2024-04-29 20:57:42 -0400

    Update interesting-queries.sql

--- interesting-queries.sql
+++ interesting-queries.sql
@@ -22,6 +22,11 @@ FROM table1
 GROUP BY ROLLUP(yr, mth) -- years + months, years only, total (hierarchy intact)
 ORDER BY 1 DESC, 2 DESC;
 
+-- in case of duplicates, get the customer that was created first according to creation_date in ascending order
+SELECT customer_id, customer_name, creation_date
+FROM customers
+QUALIFY ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY creation_date) = 1;
+
 -- Recursive cte
 -- From: https://www.linkedin.com/posts/davidkfreitag_sql-dataengineering-activity-7187786005481967616-_Py8?utm_source=share&utm_medium=member_desktop
 -- Prints 1,2,3
